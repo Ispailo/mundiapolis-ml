df2271f6bd76f30ff5b495cc8942ddd2ec833153
Fadili Ismail <80095967+Ispailo@users.noreply.github.com>
2021-03-17 23:22:30 +0100

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -9,18 +9,6 @@ class Neuron:
     """
 
     def __init__(self, nx):
-        """
-        Constructor for the class
-        Arguments:
-         - nx (int): is the number of input features to the neuron
-        Public instance attributes:
-         - W: The weights vector for the neuron. Upon instantiation, it should
-              be initialized using a random normal distribution.
-         - b: The bias for the neuron. Upon instantiation, it should be
-              initialized to 0.
-         - A: The activated output of the neuron (prediction). Upon
-              instantiation, it should be initialized to 0.
-        """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
